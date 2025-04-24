18149f78dd36a8fb8e6767823fd642d5c50d9853
Jacobtread <jacobtread@gmail.com>
2025-04-24 21:46:42 +1200

    feat: remaining device tests

--- src-tauri/src/database/entity/device.rs
+++ src-tauri/src/database/entity/device.rs
@@ -198,8 +198,10 @@ impl DeviceModel {
 
 #[cfg(test)]
 mod test {
+    use uuid::Uuid;
+
     use crate::database::{
-        entity::device::{CreateDevice, DeviceModel},
+        entity::device::{CreateDevice, DeviceModel, UpdateDevice},
         mock_database,
     };
 
@@ -244,5 +246,172 @@ mod test {
         assert_eq!(model.port, port);
         assert_eq!(model.order, order);
         assert_eq!(model.access_token, access_token);
+    }
+
+    /// Tests that deleting a device works as intended
+    #[tokio::test]
+    async fn test_delete_device() {
+        let db = mock_database().await;
+
+        // Create a model to work with
+        let model = DeviceModel::create(
+            &db,
+            CreateDevice {
+                name: "Test".to_string(),
+                host: "127.0.0.1".to_string(),
+                port: 8080,
+                order: 0,
+                access_token: Some("Test".to_string()),
+            },
+        )
+        .await
+        .unwrap();
+
+        // Delete the model
+        DeviceModel::delete(&db, model.id).await.unwrap();
+
+        // Model should no longer exist
+        let model = DeviceModel::get_by_id(&db, model.id).await.unwrap();
+        assert!(model.is_none());
+    }
+
+    /// Tests updating a device correctly updates the fields in the
+    /// returned model and in the database
+    #[tokio::test]
+    async fn test_update_device() {
+        let name = "Test".to_string();
+        let host = "127.0.0.1".to_string();
+        let port = 8080;
+        let order = 0;
+        let access_token = Some("Test".to_string());
+        let db = mock_database().await;
+
+        // Create a model to work with
+        let model = DeviceModel::create(
+            &db,
+            CreateDevice {
+                name: "Test 2".to_string(),
+                host: "127.0.0.2".to_string(),
+                port: 8081,
+                order: 1,
+                access_token: Some("Test 2".to_string()),
+            },
+        )
+        .await
+        .unwrap();
+
+        // Update the model
+        let model = model
+            .update(
+                &db,
+                UpdateDevice {
+                    name: Some(name.clone()),
+                    host: Some(host.clone()),
+                    port: Some(port),
+                    order: Some(order),
+                    access_token: Some(access_token.clone()),
+                },
+            )
+            .await
+            .unwrap();
+
+        // Ensure the returned model is correct
+        assert_eq!(model.name, name);
+        assert_eq!(model.host, host);
+        assert_eq!(model.port, port);
+        assert_eq!(model.order, order);
+        assert_eq!(model.access_token, access_token);
+
+        // Ensure the copy in the database is correct
+        let model = DeviceModel::get_by_id(&db, model.id)
+            .await
+            .unwrap()
+            .expect("model should exist");
+        assert_eq!(model.name, name);
+        assert_eq!(model.host, host);
+        assert_eq!(model.port, port);
+        assert_eq!(model.order, order);
+        assert_eq!(model.access_token, access_token);
+    }
+
+    /// Tests that getting a device by ID works
+    #[tokio::test]
+    async fn test_get_device_by_id() {
+        let name = "Test".to_string();
+        let host = "127.0.0.1".to_string();
+        let port = 8080;
+        let order = 0;
+        let access_token = Some("Test".to_string());
+
+        let db = mock_database().await;
+        let model = DeviceModel::create(
+            &db,
+            CreateDevice {
+                name: name.clone(),
+                host: host.clone(),
+                port,
+                order,
+                access_token: access_token.clone(),
+            },
+        )
+        .await
+        .unwrap();
+
+        // Ensure the created device exists
+        DeviceModel::get_by_id(&db, model.id)
+            .await
+            .unwrap()
+            .expect("model should exist");
+
+        // Ensure an unknown model doesn't exist
+        let model = DeviceModel::get_by_id(&db, Uuid::new_v4()).await.unwrap();
+        assert!(model.is_none());
+    }
+
+    /// Tests that getting a device by ID works
+    #[tokio::test]
+    async fn test_get_all_devices() {
+        #[rustfmt::skip]
+        let devices_data = [
+            ("Test".to_string(), "127.0.0.1".to_string(), 8480, 0, Some("Test 2".to_string())),
+            ("Test 1".to_string(), "127.0.0.3".to_string(), 8280, 2, Some("Test 1".to_string())),
+            ("Test 2".to_string(), "127.0.0.2".to_string(), 8082, 1, Some("Test 4".to_string())),
+            ("Test 3".to_string(), "127.0.0.4".to_string(), 8180, 5, Some("Test 2".to_string())),
+            ("Test 4".to_string(), "127.0.0.2".to_string(), 8081, 10, Some("Test 3".to_string())),
+        ];
+
+        // Prep an ordered copy of the results
+        let mut ordered_data = devices_data.to_vec();
+        ordered_data.sort_by(|a, b| a.3.cmp(&b.3));
+
+        let db = mock_database().await;
+
+        // Create the devices
+        for (name, host, port, order, access_token) in devices_data {
+            DeviceModel::create(
+                &db,
+                CreateDevice {
+                    name: name.clone(),
+                    host: host.clone(),
+                    port,
+                    order,
+                    access_token: access_token.clone(),
+                },
+            )
+            .await
+            .unwrap();
+        }
+
+        let devices = DeviceModel::all(&db).await.unwrap();
+
+        for (index, (name, host, port, order, access_token)) in ordered_data.into_iter().enumerate()
+        {
+            let model = &devices[index];
+            assert_eq!(model.name, name);
+            assert_eq!(model.host, host);
+            assert_eq!(model.port, port);
+            assert_eq!(model.order, order);
+            assert_eq!(model.access_token, access_token);
+        }
     }
 }
